4e2a958676d06b9e54ce907b5f040410aef18d54
Востриков Михаил <mikhailvost228@gmail.com>
2026-01-15 18:34:05 +0700

    Registered models in django admin panel

--- backend/events/admin.py
+++ backend/events/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Event, DateOption, Participant, Vote, Message
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
+
+
+@admin.register(DateOption)
+class DateOprionAdmin(admin.ModelAdmin):
+    list_display = ('event', 'date')
+    list_filter = ('event',)
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('username', 'event')
+    search_fields = ('name',)
+    list_filter = ('event',)
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('participant', 'date_option', 'voted_at')
+    list_filter = ('date_option',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('event', 'participant', 'created_at')
+    list_filter = ('event',)
