ce61201d624b1bf49632402e0e53f3e716be78b1
Востриков Михаил <mikhailvost228@gmail.com>
2026-01-15 18:47:31 +0700

    Added DRF serializers

---
+++ backend/events/serializers.py
@@ -0,0 +1,72 @@
+from rest_framework import serializers
+from .models import Event, DateOption, Participant, Vote, Message
+
+
+class DateOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DateOption
+        fields = ('id', 'date')
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ('id', 'username')
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender_username = serializers.CharField(
+        source = 'participant.username',
+        read_only = True
+    )
+
+    class Meta:
+        model = Message
+        fields = ('id', 'sender_name', 'text', 'created_at')
+
+
+class EventDetailSerializer(serializers.ModelSerializer):
+    date_options = DateOptionSerializer(many=True, read_only=True)
+    participants = ParticipantSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Event
+        fields = (
+            'id',
+            'title',
+            'description',
+            'created_at',
+            'date_options',
+            'participants',
+            'messages',
+        )
+
+
+class EventCreateSerializer(serializers.ModelSerializer):
+    dates = serializers.ListField(
+        child=serializers.DateField(),
+        write_only=True
+    )
+
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'description', 'dates')
+
+    def create(self, validated_data):
+        dates = validated_data.pop('dates')
+        event = Event.objects.create(**validated_data)
+
+        DateOption.objects.bulk_create([
+            DateOption(event=event, date=date)
+            for date in dates
+        ])
+
+        return event
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('participant', 'date_option')
+
