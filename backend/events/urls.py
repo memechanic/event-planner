ddd293acd8e7ea22d9e76901a874c44c2f2a4b0d
Востриков Михаил <mikhailvost228@gmail.com>
2026-01-15 19:48:42 +0700

    Added vote POST endpoint

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventCreateView, EventDetailView
+from .views import EventCreateView, EventDetailView, VoteCreateView
 
 urlpatterns = [
     path('events/', EventCreateView.as_view()),
     path('events/<uuid:id>/', EventDetailView.as_view()),
+    path('votes/', VoteCreateView.as_view()),
 ]
--- backend/events/views.py
+++ backend/events/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
-from .models import Event
-from .serializers import EventCreateSerializer, EventDetailSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from .models import Event, Vote
+from .serializers import EventCreateSerializer, EventDetailSerializer, VoteSerializer
 
 
 class EventCreateView(generics.CreateAPIView):
@@ -12,3 +14,19 @@ class EventDetailView(generics.RetrieveAPIView):
     queryset = Event.objects.all()
     serializer_class = EventDetailSerializer
     lookup_field = 'id'
+
+
+class VoteCreateView(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exceptrion=True)
+
+        Vote.objects.update_or_create(
+            participant = serializer.validated_data['participant'],
+            date_option = serializer.validated_data['date_option'],
+        )
+
+        return Response({'status': 'voted'}, status=status.HTTP_200_OK)
+
