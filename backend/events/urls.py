4f4d17c77cc2291470010cd9feca56d5201b6c73
Востриков Михаил <mikhailvost228@gmail.com>
2026-01-16 07:32:18 +0700

    Edited votes url api endpoint

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('events/<uuid:id>/', EventDetailView.as_view()),
     path('events/<uuid:event_id>/messages/', EventMessagesView.as_view()),
     path('events/<uuid:event_id>/participants/', EventParticipantView.as_view()),
-    path('votes/', VoteCreateView.as_view()),
+    path('events/<uuid:event_id>/votes/', VoteCreateView.as_view()),
 ]
